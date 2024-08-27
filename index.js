63225135b7893d381d0932d78dc6e447ed55dd7a
brunolmars <bruno.lmarques@sempreceub.com>
2024-08-27 08:54:35 -0300

    hn

---
+++ index.js
@@ -0,0 +1,6 @@
+const diasemana = document.getElementById ("dia-semana")
+const diamesano = document.getElementById ("dia-mes-ano")
+const horaminseg = document.getElementById ("dia-hora-min-seg")
+
+
+diasemana.textContent ="DOOMINGO"
