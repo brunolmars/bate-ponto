61c745643ad9b5c008a4b63afcb085fe733da9c7
brunolmars <bruno.lmarques@sempreceub.com>
2024-09-24 10:37:32 -0300

    s

--- index.js
+++ index.js
@@ -128,15 +128,3 @@ setTimeout(msgsucesso, 5000);
 printCurrentHour();
 setInterval(printCurrentHour, 1000);
 
-function updateTime() {
-    const timeElement = document.getElementById('time');
-    const now = new Date();
-    const hours = now.getHours().toString().padStart(2, '0');
-    const minutes = now.getMinutes().toString().padStart(2, '0');
-    const seconds = now.getSeconds().toString().padStart(2, '0');
-    const timeString = `${hours}:${minutes}:${seconds}`;
-    timeElement.textContent = timeString;
-}
-
-setInterval(updateTime, 1000); 
-updateTime(); 
